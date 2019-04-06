877e3feb01e15871104cf0dac37a41d437d930d6
Philippe Bourdages <philippe.bourdages@gmail.com>
2019-04-06 16:55:17 -0400

    Fix resolving issue

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,14 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
     template: "./src/index.html",
-    filename: "./index.html"
+    filename: "index.html"
 });
 
-
-
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: path.resolve(__dirname, "./src/index.tsx"),
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: "bundle.js",
-    },
-    resolve: {
-        extensions: ["js", "jsx", "ts", "tsx", "json"],
-        modules: ["node_modules"],
     },
     module: {
         rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
