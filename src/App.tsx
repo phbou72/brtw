c3410c30d9468bf0d85827bebd0a3aab37bbb0f4
Philippe Bourdages <philippe.bourdages@gmail.com>
2019-06-23 22:17:53 -0400

    Fix loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 
 const App = () => {
-    console.log("DERP");
     return <div className="app">App</div>;
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
     template: "./src/index.html",
-    filename: "index.html"
+    filename: "index.html",
 });
 
 module.exports = {
+    devtool: "inline-source-map",
     entry: path.resolve(__dirname, "./src/index.tsx"),
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, "./dist"),
         filename: "bundle.js",
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
     },
     module: {
         rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
     },
-    devtool: "inline-source-map",
-    plugins: [htmlPlugin]
+    plugins: [htmlPlugin],
 };
