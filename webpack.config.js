b9f414564ed29a7f8e9bca196dbccabc6322517a
Philippe Bourdages <philippe.bourdages@gmail.com>
2020-04-01 22:30:46 -0400

    Change plugins

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,20 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ForkTsCheckerNotifierWebpackPlugin = require("fork-ts-checker-notifier-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const createStyledComponentsTransformer = require("typescript-plugin-styled-components").default;
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 // const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const styledComponentsTransformer = createStyledComponentsTransformer();
 
-const htmlPlugin = new HtmlWebPackPlugin({
-    template: "src/index.html",
-    hash: true,
-});
-
 module.exports = {
     entry: path.resolve(__dirname, "./src/index.tsx"),
+    cache: false,
     devtool: "eval-cheap-module-source-map", // inline-source-map is the default
     output: {
         path: path.resolve(__dirname, "./dist"),
-        filename: "bundle.js",
+        filename: "[name].bundle.js",
+        chunkFilename: "[name].bundle.js",
     },
     resolve: {
         extensions: [".ts", ".tsx", ".js", ".jsx"],
@@ -47,12 +46,29 @@ module.exports = {
             },
         ],
     },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true, // Must be set to true if using source-maps in production
+                terserOptions: {
+                    // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+                },
+            }),
+        ],
+    },
     plugins: [
-        htmlPlugin,
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({
+            template: "src/index.html",
+            hash: true,
+        }),
         new ForkTsCheckerWebpackPlugin({
             eslint: true,
         }),
         new ForkTsCheckerNotifierWebpackPlugin({ title: "TypeScript", excludeWarnings: false }),
-        // new BundleAnalyzerPlugin()
+        // new BundleAnalyzerPlugin(),
     ],
 };
