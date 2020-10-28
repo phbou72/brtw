bde76ee4d6d42c1b423864ecbeba815bd6ed57e1
Philippe Bourdages <philippe.bourdages@gmail.com>
2020-10-27 21:42:12 -0400

    Upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -65,9 +65,7 @@ module.exports = function (env) {
         optimization: {
             minimizer: [
                 new TerserPlugin({
-                    cache: true,
                     parallel: true,
-                    sourceMap: true, // Must be set to true if using source-maps in production
                     terserOptions: {
                         // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
                     },
